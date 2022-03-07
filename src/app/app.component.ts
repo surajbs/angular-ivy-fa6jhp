7472c9bdf37ed439016a5e249792f6b8f68a4b2e
surajbs <surajbs91@gmail.com>
2022-03-07 17:53:52 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  data = 'Royal Challengers Bangalore';
+
+  constructor() {}
+
+  ngOnInit() {
+    // Both of the below mentioned method works for Data
+    //this.getData();
+    this.getDataset();
+  }
+
+  getData() {
+    // getData - finds the match from data and breaks the given string and prints the output.
+    const output = this.data.match(/\b(\w)/g).join('');
+    console.log(output);
+  }
+
+  getDataset() {
+    //getDataset - splits the string by searching with spaces and finds first index char and prints the output
+    const output = this.data
+      .split(/\s/)
+      .reduce((response, word) => (response += word.slice(0, 1)), '');
+    console.log(output);
+  }
 }
